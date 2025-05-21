47444c69c018c4e7fe59034f6c99a5c5af673f89
Masanao Daimon <diams.rider@gmail.com>
2025-05-21 22:47:09 +0900

    Disable manual resizing for textarea in ChatInput component.

--- gpt-chatbot-web-ui/src/components/ui/chat_input.tsx
+++ gpt-chatbot-web-ui/src/components/ui/chat_input.tsx
@@ -19,7 +19,7 @@ export default function ChatInput() {
         value={value}
         onChange={(e) => setValue(e.target.value)}
         rows={1}
-        className="border break-words bg-white rounded-xl dark:bg-gray-500 p-2 w-[min(90%,750px)]"
+        className="border resize-none break-words bg-white rounded-xl dark:bg-gray-500 p-2 w-[min(90%,750px)]"
         placeholder={t("Type your question here.")}
       />
     </div>
