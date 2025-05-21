f9db789098856d813a5a352fafb6b344b59a7b69
Masanao Daimon <diams.rider@gmail.com>
2025-05-21 19:43:03 +0900

    Add ChatInput component.

--- gpt-chatbot-web-ui/src/app/[lang]/page.tsx
+++ gpt-chatbot-web-ui/src/app/[lang]/page.tsx
@@ -1,4 +1,5 @@
 import { initTranslations } from "@/lib/i18n";
+import ChatInput from "@/components/chat_input";
 
 type HomePagePromiseProps = {
   lang: string;
@@ -14,5 +15,9 @@ export default async function Home(props: HomePageProps) {
     locale: lang,
     namespaces: ["temporary"],
   });
-  return <main>{t("This is the English locale.")}</main>;
+  return (
+    <main>
+      <ChatInput />
+    </main>
+  );
 }
---
+++ gpt-chatbot-web-ui/src/components/chat_input.tsx
@@ -0,0 +1,3 @@
+export default function ChatInput() {
+  return <div>This is ChatInput Component</div>;
+}
