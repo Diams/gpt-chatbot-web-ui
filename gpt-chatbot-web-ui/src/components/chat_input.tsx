5fc57475ea4d4d45ff956e9b81581a8683def6bf
Masanao Daimon <diams.rider@gmail.com>
2025-05-21 21:18:15 +0900

    Add textare tag in ChatInput component.

--- gpt-chatbot-web-ui/src/components/chat_input.tsx
+++ gpt-chatbot-web-ui/src/components/chat_input.tsx
@@ -3,11 +3,14 @@ import { initTranslations } from "@/lib/i18n";
 export default async function ChatInput({ lang }: { lang: string }) {
   const { t } = await initTranslations({
     locale: lang,
-    namespaces: ["temporary"],
+    namespaces: ["chat_room"],
   });
   return (
-    <div className="fixed bottom-0 left-0 w-full">
-      {t("This is ChatInput component.")}
+    <div className="flex fixed bottom-0 left-0 w-full py-10 justify-center">
+      <textarea
+        className="border break-words bg-white rounded-xl dark:bg-gray-500 p-2 w-[min(90%,750px)]"
+        placeholder={t("Type your question here.")}
+      />
     </div>
   );
 }
