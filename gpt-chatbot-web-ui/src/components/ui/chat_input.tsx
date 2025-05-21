74924612bea4ff27a2b4c02c518d3b51d885beed
Masanao Daimon <diams.rider@gmail.com>
2025-05-21 23:35:10 +0900

    Update to use react-textarea-autosize instead of plane textarea tag.

--- gpt-chatbot-web-ui/src/components/ui/chat_input.tsx
+++ gpt-chatbot-web-ui/src/components/ui/chat_input.tsx
@@ -1,37 +1,20 @@
 "use client";
 
-import { useState, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
+import TextareaAutosize from "react-textarea-autosize";
 
 export default function ChatInput() {
   const { t } = useTranslation();
-  const textarea_ref = useRef<HTMLTextAreaElement>(null);
-  const [value, setValue] = useState("");
-  useEffect(() => {
-    if (textarea_ref.current) {
-      ResizeTextarea(textarea_ref.current);
-    }
-  }, [value]);
   return (
     <div className="flex fixed bottom-0 left-0 w-full py-10 justify-center">
       <div className="flex w-[min(90%,750px)] bg-white dark:bg-gray-500 p-2 border rounded-xl">
-        <textarea
-          ref={textarea_ref}
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          rows={1}
-          className="flex-grow resize-none break-words bg-white dark:bg-gray-500 focus:outline-none"
+        <TextareaAutosize
+          maxRows={10}
+          minRows={1}
           placeholder={t("Type your question here.")}
+          className="flex-grow resize-none break-word focus:outline-none"
         />
       </div>
     </div>
   );
-}
-
-function ResizeTextarea(textarea: HTMLTextAreaElement) {
-  textarea.style.height = "auto";
-  const scroll_height = textarea.scrollHeight;
-  const line_height = 24;
-  const max_height = line_height * 10;
-  textarea.style.height = `${Math.min(scroll_height, max_height)}px`;
 }
