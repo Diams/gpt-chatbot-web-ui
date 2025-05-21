3b1ec52edb66276931a3479f7a3c3d0371530427
Masanao Daimon <diams.rider@gmail.com>
2025-05-22 00:22:09 +0900

    Add send button to ChatInput component.

--- gpt-chatbot-web-ui/src/components/ui/chat_input.tsx
+++ gpt-chatbot-web-ui/src/components/ui/chat_input.tsx
@@ -1,18 +1,26 @@
 "use client";
 
+import { IconSend } from "@tabler/icons-react";
 import { useTranslation } from "react-i18next";
 import TextareaAutosize from "react-textarea-autosize";
 
 export default function ChatInput() {
   const { t } = useTranslation();
   return (
-    <div className="flex fixed bottom-0 left-0 w-full py-10 justify-center">
+    <div className="flex fixed bottom-0 left-0 w-full py-10 justify-center items-center px-2">
       <div className="flex w-[min(90%,750px)] bg-white dark:bg-gray-500 p-2 border rounded-xl">
         <TextareaAutosize
           maxRows={10}
           minRows={1}
           placeholder={t("Type your question here.")}
           className="flex-grow resize-none break-word focus:outline-none"
+        />
+      </div>
+      <div className="border-2 rounded-full p-2 ml-2 cursor-pointer transition-colors hover:bg-gray-900 hover:text-gray-200 dark:hover:bg-gray-200 dark:hover:text-gray-900 active:scale-90">
+        <IconSend
+          onClick={() => {
+            console.log("send.");
+          }}
         />
       </div>
     </div>
