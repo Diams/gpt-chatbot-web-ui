bc53552f25ae797e53ff97616a425cb281e156da
Masanao Daimon <diams.rider@gmail.com>
2025-05-22 23:04:24 +0900

    [Update]TextareAutosizeについて状態を管理するようにした

--- gpt-chatbot-web-ui/src/components/ui/chat_input.tsx
+++ gpt-chatbot-web-ui/src/components/ui/chat_input.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { IconSend } from "@tabler/icons-react";
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import TextareaAutosize from "react-textarea-autosize";
 
 export default function ChatInput() {
   const { t } = useTranslation();
+  const [input_value, set_input] = useState("");
   return (
     <div className="flex fixed bottom-0 left-0 w-full py-10 justify-center items-center px-2">
       <div className="flex w-[min(90%,750px)] bg-white dark:bg-gray-500 p-2 border rounded-xl">
@@ -13,6 +15,8 @@ export default function ChatInput() {
           maxRows={10}
           minRows={1}
           placeholder={t("Type your question here.")}
+          value={input_value}
+          onChange={(e) => set_input(e.target.value)}
           className="flex-grow resize-none break-word focus:outline-none"
         />
       </div>
