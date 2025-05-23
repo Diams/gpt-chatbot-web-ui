b95b5ef59fe32cfacddf47a0e60cfb699f6ea539
Masanao Daimon <diams.rider@gmail.com>
2025-05-23 23:46:07 +0900

    Update to refer programming languages map on calling GetExtension.

--- gpt-chatbot-web-ui/src/lib/utils.ts
+++ gpt-chatbot-web-ui/src/lib/utils.ts
@@ -2,46 +2,33 @@ interface languageMap {
   [key: string]: string | undefined;
 }
 
-export function GetExtension(langueage: string): string {
-  console.log(langueage);
-  switch (langueage) {
-    case "bash":
-      return "sh";
-    case "css":
-      return "css";
-    case "html":
-      return "html";
-    case "jsx":
-      return "jsx";
-    case "python":
-      return "py";
-    default:
-      return "txt";
-  }
-}
-
-export const programmingLanguages: languageMap = {
-  c: ".c",
-  cpp: ".cpp",
-  css: ".css",
-  "c++": ".cpp",
-  "c#": ".cs",
-  go: ".go",
-  haskell: ".hs",
-  html: ".html",
-  java: ".java",
-  javascript: ".js",
-  kotlin: ".kt",
-  lua: ".lua",
-  "objective-c": ".m",
-  perl: ".pl",
-  php: ".php",
-  python: ".py",
-  ruby: ".rb",
-  rust: ".rs",
-  scala: ".scala",
-  shell: ".sh",
-  sql: ".sql",
-  swift: ".swift",
-  typescript: ".ts",
+const programmingLanguages: languageMap = {
+  c: "c",
+  cpp: "cpp",
+  csharp: "cs",
+  css: "css",
+  "c++": "cpp",
+  "c#": "cs",
+  go: "go",
+  haskell: "hs",
+  html: "html",
+  java: "java",
+  javascript: "js",
+  kotlin: "kt",
+  lua: "lua",
+  "objective-c": "m",
+  perl: "pl",
+  php: "php",
+  python: "py",
+  ruby: "rb",
+  rust: "rs",
+  scala: "scala",
+  shell: "sh",
+  sql: "sql",
+  swift: "swift",
+  typescript: "ts",
 };
+
+export function GetExtension(langueage: string): string {
+  return programmingLanguages[langueage] ?? "txt";
+}
