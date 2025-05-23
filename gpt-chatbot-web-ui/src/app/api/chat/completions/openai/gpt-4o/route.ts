d2dd48ca3595d888f45a33f816aa1a91b9c72487
Masanao Daimon <diams.rider@gmail.com>
2025-05-23 13:55:08 +0900

    Update to request to Open AI API.

--- gpt-chatbot-web-ui/src/app/api/chat/completions/openai/gpt-4o/route.ts
+++ gpt-chatbot-web-ui/src/app/api/chat/completions/openai/gpt-4o/route.ts
@@ -1,6 +1,20 @@
 import { NextRequest } from "next/server";
+import OpenAI from "openai";
+import { OpenAiApiKey } from "@/lib/models/api_key_provider";
+
+const client = new OpenAI({ apiKey: OpenAiApiKey });
 
 export async function POST(request: NextRequest): Promise<Response> {
-  await new Promise((resolve) => setTimeout(resolve, 3000));
-  return Response.json({ answer: "Answer from some GPT model." });
+  const { prompt } = await request.json();
+  const completion = await client.chat.completions.create({
+    model: "gpt-4o",
+    messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+  });
+  const answer = completion.choices[0].message.content ?? "";
+  return Response.json({ answer: answer });
 }
