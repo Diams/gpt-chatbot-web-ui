27f9a4c8f3ceaf012fad0c55d4ef07353ebd6c69
Masanao Daimon <diams.rider@gmail.com>
2025-05-23 11:57:29 +0900

    Update to display new conversations in Conversations component.

--- gpt-chatbot-web-ui/src/components/ui/chat_room/chat_input.tsx
+++ gpt-chatbot-web-ui/src/components/ui/chat_room/chat_input.tsx
@@ -13,7 +13,6 @@ export default function ChatInput() {
   const handle_send = () => {
     if (input_value !== "") {
       chat_room.Chat(input_value);
-      console.log(chat_room.Conversations);
       set_input("");
     }
   };
--- gpt-chatbot-web-ui/src/components/ui/chat_room/conversations.tsx
+++ gpt-chatbot-web-ui/src/components/ui/chat_room/conversations.tsx
@@ -1,14 +1,27 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useChatRoom } from "@/components/providers/chat_room_provider";
 import ChatMessageUI from "./conversations/chat_message_ui";
+import ChatMessage from "@/lib/chat/chat_message";
 
 export default function Conversations() {
   const chat_room = useChatRoom();
   const [conversations_value, set_conversations] = useState([
     ...chat_room.Conversations,
   ]);
+  useEffect(() => {
+    const added_conversation_listener = (new_message: ChatMessage) => {
+      set_conversations((prev_conversations: ChatMessage[]) => [
+        ...prev_conversations,
+        new_message,
+      ]);
+    };
+    chat_room.on("added_conversation", added_conversation_listener);
+    return () => {
+      chat_room.off("added_conversation", added_conversation_listener);
+    };
+  }, []);
   return (
     <div>
       {conversations_value.map((conversation, index) => (
