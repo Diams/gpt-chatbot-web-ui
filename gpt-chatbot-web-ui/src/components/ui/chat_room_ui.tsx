9fb91ba38d63809262e186387e84da3f5aa7c37c
Masanao Daimon <diams.rider@gmail.com>
2025-05-23 12:07:11 +0900

    Remove dummy default conversations.

--- gpt-chatbot-web-ui/src/components/ui/chat_room_ui.tsx
+++ gpt-chatbot-web-ui/src/components/ui/chat_room_ui.tsx
@@ -6,24 +6,7 @@ import ChatInput from "./chat_room/chat_input";
 import Conversations from "./chat_room/conversations";
 
 export default function ChatRoomUI() {
-  const chat_room = new ChatRoom([
-    {
-      role: "user",
-      message: "Hello.",
-    },
-    {
-      role: "assistant",
-      message: "Hi there! How are you?",
-    },
-    {
-      role: "user",
-      message: "I'm good, thanks. And you?",
-    },
-    {
-      role: "assistant",
-      message: "I'm doing well, too.",
-    },
-  ]);
+  const chat_room = new ChatRoom([]);
   return (
     <ChatRoomProvider chat_room_instance={chat_room}>
       <Conversations />
