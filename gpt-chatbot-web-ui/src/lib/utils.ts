748037b343061eff2b42f434cea2fddbf6ba2482
Masanao Daimon <diams.rider@gmail.com>
2025-05-23 18:03:04 +0900

    Add convertable extensions.

--- gpt-chatbot-web-ui/src/lib/utils.ts
+++ gpt-chatbot-web-ui/src/lib/utils.ts
@@ -1,6 +1,10 @@
 export function GetExtension(langueage: string): string {
   console.log(langueage);
   switch (langueage) {
+    case "bash":
+      return "sh";
+    case "jsx":
+      return "jsx";
     case "python":
       return "py";
     default:
