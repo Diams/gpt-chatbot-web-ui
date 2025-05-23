550e168438f838e57d747db7117351c5f9a0049f
Masanao Daimon <diams.rider@gmail.com>
2025-05-23 11:40:12 +0900

    Update to manage ChatRoom.Conversation using useState() hook.

--- gpt-chatbot-web-ui/src/components/ui/chat_room/conversations.tsx
+++ gpt-chatbot-web-ui/src/components/ui/chat_room/conversations.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { useState } from "react";
 import { useChatRoom } from "@/components/providers/chat_room_provider";
 import ChatMessageUI from "./conversations/chat_message_ui";
 
 export default function Conversations() {
-  const dummy_conversations = useChatRoom().Conversations;
+  const chat_room = useChatRoom();
+  const [conversations_value, set_conversations] = useState([
+    ...chat_room.Conversations,
+  ]);
   return (
     <div>
-      {dummy_conversations.map((conversation, index) => (
+      {conversations_value.map((conversation, index) => (
         <ChatMessageUI
           key={index}
           role={conversation.role}
