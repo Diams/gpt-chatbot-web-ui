55ceb6765a79e574cb8ae84703a9963c19585fe5
Masanao Daimon <diams.rider@gmail.com>
2025-06-01 00:04:54 +0900

    [Feature] Add model label translation and update ModelSelector component

--- gpt-chatbot-web-ui/src/components/ui/chatbot/chat_room/model_selector.tsx
+++ gpt-chatbot-web-ui/src/components/ui/chatbot/chat_room/model_selector.tsx
@@ -1,9 +1,11 @@
+import { useTranslation } from "react-i18next";
 import { Models } from "@/lib/models/models";
 
 export default function ModelSelector() {
+  const { t } = useTranslation();
   return (
     <div className="flex flex-row gap-2">
-      <div>model:</div>
+      <div>{t("model")}:</div>
       <select className="w-[200px] bg-white dark:bg-gray-500 rounded-md">
         {Models.map((model, index) => (
           <option key={index} value={`${model.provider}/${model.model_name}`}>
