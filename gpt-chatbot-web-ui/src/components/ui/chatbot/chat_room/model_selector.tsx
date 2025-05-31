910379467a8ee574d00611e9138b49c7e7bae0ce
Masanao Daimon <diams.rider@gmail.com>
2025-05-31 23:53:46 +0900

    [Feature] Populate ModelSelector dropdown with available models

--- gpt-chatbot-web-ui/src/components/ui/chatbot/chat_room/model_selector.tsx
+++ gpt-chatbot-web-ui/src/components/ui/chatbot/chat_room/model_selector.tsx
@@ -1,8 +1,16 @@
+import { Models } from "@/lib/models/models";
+
 export default function ModelSelector() {
   return (
     <div className="flex flex-row gap-2">
       <div>model: </div>
-      <select className="w-[200px] bg-white dark:bg-gray-500 rounded-md"></select>
+      <select className="w-[200px] bg-white dark:bg-gray-500 rounded-md">
+        {Models.map((model, index) => (
+          <option key={index} value={`${model.provider}/${model.model_name}`}>
+            {`${model.provider}/${model.model_name}`}
+          </option>
+        ))}
+      </select>
     </div>
   );
 }
