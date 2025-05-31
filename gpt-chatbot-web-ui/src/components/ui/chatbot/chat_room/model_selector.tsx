7743deb91364c3b7d93a7aacf9a080d12116371c
Masanao Daimon <diams.rider@gmail.com>
2025-05-31 23:54:45 +0900

    Fix formatting of model label in ModelSelector component

--- gpt-chatbot-web-ui/src/components/ui/chatbot/chat_room/model_selector.tsx
+++ gpt-chatbot-web-ui/src/components/ui/chatbot/chat_room/model_selector.tsx
@@ -3,7 +3,7 @@ import { Models } from "@/lib/models/models";
 export default function ModelSelector() {
   return (
     <div className="flex flex-row gap-2">
-      <div>model: </div>
+      <div>model:</div>
       <select className="w-[200px] bg-white dark:bg-gray-500 rounded-md">
         {Models.map((model, index) => (
           <option key={index} value={`${model.provider}/${model.model_name}`}>
